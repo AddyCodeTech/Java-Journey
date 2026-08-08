eb4a316742231ec4ee34cb89f26b9fd324c2b9c6
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-08 19:04:39 +0530

    sum

---
+++ Loops/sum.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class sum {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for(int i=1; i<=10; i++){
+            sum = sum + 2;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
