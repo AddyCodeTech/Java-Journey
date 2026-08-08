99cbcb6fdf649ab1c826594e7a778f433d67e7e5
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-08 21:02:41 +0530

    Multiplication Table

---
+++ Loops/multiplicationtable.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class multiplicationtable {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=10;i++){
+            System.out.println(n + " X " + i + " = " + (n*i));
+        }
+        
+        sc.close();
+    }
+}
