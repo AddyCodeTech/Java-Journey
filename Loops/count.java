f09300636cc0db1108bf10c16c69da01dec2047d
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-08 19:00:31 +0530

    counter

---
+++ Loops/count.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class count {
+    public static void main(String[] args) {
+        int size = 10;
+        for(int i = 1; i<=size; i++){
+            System.out.println(i);
+        }
+    }
+}
