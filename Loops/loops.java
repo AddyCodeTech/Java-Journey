1d9ff3d632b74ac5cf96ff41d28ef77f842b3984
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-08 21:25:28 +0530

    Loops

---
+++ Loops/loops.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class loops {
+    public static void main(){
+        int n = 1;
+        for(int i=1;i<=5;i++){
+            System.out.println("Hello Java!");
+        }
+    }
+}
