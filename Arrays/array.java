342bb7931d9048449c59af34eebc8bcbaea83fd1
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-09 20:55:37 +0530

    Arrays

---
+++ Arrays/array.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class array{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+        int[] ar = new int[size];
+        // initializing array
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the elements: ");
+            ar[i]=sc.nextInt();
+        }
+        System.out.print("\n\nDisplay Elements: ");
+        // display array
+        for(int i = 0; i<size; i++){
+             System.out.print(ar[i]+" ");
+        }
+        sc.close();
+    }
+}
