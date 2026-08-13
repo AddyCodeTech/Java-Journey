9260b5aeddfc6ec68f0573e96c57653ae301811d
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-13 21:37:49 +0530

    Initialize an Object

---
+++ Class/object.java
@@ -0,0 +1,11 @@
+//OOPS 1: TO CREATE AN OBJECT
+package Class;
+
+public class object {
+int x = 5;
+
+public static void main(String[] args){
+    object myObj = new object();
+    System.out.println(myObj.x);
+}
+}
