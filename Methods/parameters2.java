cb72afb0a444c64b86b04a27833e1235b9750054
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-13 20:49:23 +0530

    Multiple Parameters

---
+++ Methods/parameters2.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class parameters2 {
+    static void intro(String name, int age){
+        System.out.println(name + " is " + age + " years old!");
+    }
+    public static void main(String[] args){
+        intro("Aditya", 21);
+        intro("Abhishek", 22);
+        intro("Aman", 21);
+    }
+}
