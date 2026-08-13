ef8ff0e7cdb0bcb149b3d2a878a91d18d57c89af
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-13 21:02:17 +0530

    Return Value

---
+++ Methods/returndemo.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class returndemo {
+    static int intro(int x){
+        return x + 10;
+    }
+
+    public static void main(String[] args){
+        System.out.println(intro(12));
+    }
+}
