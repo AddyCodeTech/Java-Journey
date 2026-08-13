574a3a7d3b31a128b1efc55d322471d22a095bb5
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-13 21:05:02 +0530

    Two Parameter Return

---
+++ Methods/return2demo.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class return2demo {
+    static int intro(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args){
+        System.out.println(intro(23,37));
+    }
+}
