4bced56beb2b3778a856a7bc94fe2c5122bf7ee0
AddyCodeTech <adityasinghffjuvi@gmail.com>
2026-08-13 20:44:49 +0530

    Parameters

---
+++ Methods/parameters.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class parameters{
+    static void intro(String name){
+        System.out.println(name + ", Welcome!");
+    }
+    public static void main(){
+        intro("Aditya");
+        intro("Abhishek");
+        intro("Aman");
+    }
+}
